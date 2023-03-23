e65094437df713986b7016f5a01b27de0a35b5ab
Fun741 <89957391+Fun741@users.noreply.github.com>
2023-03-22 20:29:37 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,10 +1,10 @@
 <?php
-    $dsn = 'mysql:host=localhost; dbname=zippyusedautos';
-    $username = 'root';
-    //$password = '';
+    $dsn = 'mysql:host=r4wkv4apxn9btls2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; dbname=r7g8fyyovht5zmwf';
+    $username = 'o72xq0azop2f0fzj';
+    $password = 'ho14plukfvyp73sf';
 
     try {
-        $db = new PDO($dsn, $username);
+        $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error_message = 'Database error';
         $error_message .= $e->getMessage();
